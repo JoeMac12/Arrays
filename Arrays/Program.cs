ac92995703b4638e8d3681a6294995884f39e03c
Joseph <josephmacdonald4629@gmail.com>
2023-10-30 09:27:13 -0300

    learn arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,6 +10,72 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrays");
+            Console.WriteLine("------");
+            Console.WriteLine();
+
+            // single weapon
+            // weapon ammo amount
+
+            //int ammo;
+            //ammo = 0;
+            // Fire()
+            // Reload()
+            // Range Check (0.50)
+
+
+            //int pistolAmmo;
+            //int shotGun;
+            //int sprederAmmo;
+            //int rocketLauncherAmmo;
+            //string currentWeapon;
+            // Current Weapon; 0 = pistol, 1 = shotgun, ect
+            // Fire()
+
+            // if (currentWeapon == "pistol") ....
+            // {
+            //      // range check
+            //pistolAmmo = pistolAmmo - 1;
+            // }
+            // else if (currentWeapon == "shotGun") ....
+
+
+
+            // multiple weapons w/arrays
+            // 0 - pistol
+            // 1 - shotgun
+            // 2 - spreader
+            // 3 - rocket launcher
+
+
+            int[] ammo; // declaration
+                        // "int[]" = type = "array of ints'
+            ammo = new int[4]; // instatiation (New!) -- make sure there is memory!
+            // max ammo cheat
+
+            ammo[0] = 50;// pistol
+            ammo[1] = 20;// shotgun
+            ammo[2] = 100;// spreader
+            ammo[3] = 6;// rocket launcher
+            //ammo[4] = 0;// initialization - does not work! since it's out of range of the array
+
+            int currentWeapon;
+            currentWeapon = 0; // pistol
+            //Fire()
+            // PickUp(int weaponType)
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+
+        static void Fire()
+        {
+            if (currentWeapon == 0)
+            {
+                ammo[1] = ammo[1] - 1;
+            }
         }
     }
 }
